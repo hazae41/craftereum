f8d5dbbef6bdeccb0710666fdfb067daf1b7babf
Haz Æ 41 <hazae41@gmail.com>
2020-11-28 19:19:31 +0100

    Update Craftereum.sol

--- Craftereum.sol
+++ Craftereum.sol
@@ -1,13 +1,18 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.7.0 <0.8.0;
-pragma experimental ABIEncoderV2;
 
 contract Listener {
     function onkill(
-        uint eventid,
+        uint _eventid,
         string memory _killer,
         string memory _target
+    ) external virtual {}
+    
+    function ontransfer(
+        uint _eventid,
+        uint _amount,
+        string memory _player
     ) external virtual {}
 }
 
@@ -30,19 +35,70 @@ contract Craftereum {
         delete ids[eventid];
     }
     
-    event Kill(
-        uint eventid,
-        string[] killers,
-        string[] targets
+    event Transfer(
+        uint amount,
+        string player
     );
     
+    /**
+     * Transfer msg.value amount of blockchain EMRLD to the given player
+     **/
+    function transfer(
+        string memory player
+    ) public payable {
+        emit Transfer(
+            msg.value,
+            player
+        );
+    }
+    
+    event OnTransfer(
+        uint eventid,
+        string player
+    );
+    
+    /**
+     * Trigger ontransfer when player transfers ingame EMRLD to your contract
+     * Set player to "" to include any player
+     **/
+    function ontransfer(
+      string memory player
+    ) public returns (uint){
+        uint eventid = lastid++;
+        ids[eventid] = msg.sender;
+        
+        emit OnTransfer(
+            eventid, 
+            player
+        );
+        
+        return eventid;
+    }
+    
+    event OnKill(
+        uint eventid,
+        string killer,
+        string target
+    );
+    
+    /**
+     * Trigger onkill when killer kills target
+     * Set killer to "" to include any player
+     * Set target to "" to include any player
+     **/
     function onkill(
-        string[] memory killers, 
-        string[] memory targets
+        string memory killer, 
+        string memory target
     ) public returns (uint) {
         uint eventid = lastid++;
         ids[eventid] = msg.sender;
-        emit Kill(eventid, killers, targets);
+        
+        emit OnKill(
+            eventid, 
+            killer,
+            target
+        );
+        
         return eventid;
     }
 }
