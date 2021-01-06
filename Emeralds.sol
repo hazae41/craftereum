ce06224613f5a8eda583ccdf7cfa60fa08a5291e
hazae41 <hazae41@gmail.com>
2021-01-06 22:13:22 +0100

    iemeralds.sol

--- Emeralds.sol
+++ Emeralds.sol
@@ -3,19 +3,9 @@
 pragma solidity >=0.6.0 <0.8.0;
 
 import "./Craftereum.sol";
+import "./IEmeralds.sol";
 
-import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/master/contracts/token/ERC20/IERC20.sol";
 import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/master/contracts/token/ERC20/ERC20.sol";
-
-abstract contract IEmeralds is IERC20 {
-    Craftereum public craftereum;
-
-    function balance() external virtual view returns (uint);
-
-    function mint(address account, uint amount) external virtual returns (bool);
-
-    function burn(address account, uint amount) external virtual returns (bool);
-}
 
 contract Emeralds is ERC20("Emeralds", "EMRLD"), IEmeralds {
     
---
+++ IEmeralds.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.6.0 <0.8.0;
+
+import "./Craftereum.sol";
+
+import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/master/contracts/token/ERC20/IERC20.sol";
+
+abstract contract IEmeralds is IERC20 {
+    Craftereum public craftereum;
+
+    function balance() external virtual view returns (uint);
+
+    function mint(address account, uint amount) external virtual returns (bool);
+
+    function burn(address account, uint amount) external virtual returns (bool);
+}
